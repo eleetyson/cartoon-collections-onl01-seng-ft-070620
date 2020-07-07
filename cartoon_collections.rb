319b37aa7bf0f1e30783faf3faf873dd41dc93dc
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-07 21:42:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(arr)
-  arr.each_with_index {|dwarf, index| "#{index} #{dwarf}"}
+  arr.each_with_index {|dwarf, index| "#{index}. #{dwarf}"}
 end
 
 def summon_captain_planet# code an argument here
