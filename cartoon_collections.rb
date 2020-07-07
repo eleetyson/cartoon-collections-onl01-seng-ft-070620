225284e399e8040af478d7d902ac2ea21deb751a
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-07 21:55:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  new1 = arr.map {|i| i.capitalize()}
+  new1 = arr.map {|i| i.capitalize}
   return new2 = new1.map {|j| j << "!"}
 end
 
