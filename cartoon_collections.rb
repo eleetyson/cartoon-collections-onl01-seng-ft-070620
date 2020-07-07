86b366730be61fcf7232fd8583ac8cf6ec8148fa
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-07 21:44:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,15 +2,15 @@ def roll_call_dwarves(arr)
   arr.each_with_index {|dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet()
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
